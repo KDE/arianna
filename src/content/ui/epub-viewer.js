2dfc015a49706d08cf617996b4eef12f652fd38e
Carl Schwan <carl@carlschwan.eu>
2023-02-25 12:46:35 +0100

    Fix reuse
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- src/content/ui/epub-viewer.js
+++ src/content/ui/epub-viewer.js
@@ -1,17 +1,6 @@
-/*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: 2022 Carl Schwan <carl@carlschwan.eu>
+// SPDX-FileCopyrightText: 2022 John Factotum <50942278+johnfactotum@users.noreply.github.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 // 1024 characters per page is used by Adobe Digital Editions
 const CHARACTERS_PER_PAGE = 1024
