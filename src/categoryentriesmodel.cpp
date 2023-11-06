7c5381716dbc40013f7e56e88684864ddbbe7d13
Laurent Montel <montel@kde.org>
2023-11-06 19:34:52 +0100

    Use std::as_const

--- src/categoryentriesmodel.cpp
+++ src/categoryentriesmodel.cpp
@@ -261,7 +261,7 @@ void CategoryEntriesModel::addCategoryEntry(const QString &categoryName, const B
             desiredCategory = categoryName.left(splitPos);
         }
         CategoryEntriesModel *categoryModel = nullptr;
-        for (CategoryEntriesModel *existingModel : qAsConst(d->categoryModels)) {
+        for (CategoryEntriesModel *existingModel : std::as_const(d->categoryModels)) {
             if (QString::compare(existingModel->name(), desiredCategory, Qt::CaseInsensitive) == 0) {
                 categoryModel = existingModel;
                 break;
